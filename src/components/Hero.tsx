c601b2143853ac611718807f6c1d22af9189785f
Mairinger <xstor@email.cz>
2025-12-30 21:34:37 +0000

    Change customer count and language in Hero component
    Updated text to reflect new customer count and language.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -61,8 +61,8 @@ export const Hero = () => {
       <Container>
         <div className="flex flex-col justify-center">
           <div className="text-xl text-center text-gray-700 dark:text-white">
-            Trusted by <span className="text-indigo-600">2000+</span>{" "}
-            customers worldwide
+            Spolehliví deploy pro <span className="text-indigo-600">100+</span>{" "}
+            firem a projektů
           </div>
 
           <div className="flex flex-wrap justify-center gap-5 mt-10 md:justify-around">
