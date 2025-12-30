1fff752ef8d18c62d639f79936bd640cd1fa7b06
Mairinger <xstor@email.cz>
2025-12-30 20:22:35 +0000

    Update logo image source in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ export const Navbar = () => {
           <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
               <span>
                 <Image
-                  src="/img/logo.svg"
+                  src="/img/logo-xlop.png"
                   width="32"
                   alt="N"
                   height="32"
